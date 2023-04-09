791b955f3df3342a15afa19f009e5efae35a868c
Hirqs <569543446@qq.com>
2023-04-09 20:41:07 +0800

    创建统一异常处理器-特定异常处理

--- common/service-util/src/main/java/com/rqs/common/exception/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/rqs/common/exception/GlobalExceptionHandler.java
@@ -14,4 +14,12 @@ public class GlobalExceptionHandler {
         e.printStackTrace();
         return Result.fail().message("执行全局异常处理...");
     }
+
+    //特定异常处理，执行的方法
+    @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody
+    public Result error(ArithmeticException e){
+        e.printStackTrace();
+        return Result.fail().message("执行了特定异常处理");
+    }
 }
