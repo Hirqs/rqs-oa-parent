6418bc398d25be310ea8abe5c41263178d2855f8
Hirqs <569543446@qq.com>
2023-04-09 18:16:40 +0800

    整合knife4j，knife4j是为Java MVC框架集成Swagger生成Api文档的增强解决方案。

--- service-oa/src/main/java/com/rqs/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/rqs/auth/controller/SysRoleController.java
@@ -3,6 +3,8 @@ package com.rqs.auth.controller;
 import com.rqs.auth.service.SysRoleService;
 import com.rqs.common.result.Result;
 import com.rqs.model.system.SysRole;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "角色管理接口")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -27,8 +30,8 @@ public class SysRoleController {
 //        return list;
 //    }
 
-    //查询所有角色
     //http://localhost:8800/admin/system/sysRole/findAll
+    @ApiOperation("查询所有角色")
     @GetMapping("/findAll")
     public Result findAll() {
         List<SysRole> list = sysRoleService.list();
