3c04bacaeea044d42457213a5f2d7b5ce0ef7b5a
Hirqs <569543446@qq.com>
2023-04-09 17:56:51 +0800

    @MapperScan

--- service-oa/src/main/java/com/rqs/auth/ServiceAuthApplication.java
+++ service-oa/src/main/java/com/rqs/auth/ServiceAuthApplication.java
@@ -1,9 +1,11 @@
 package com.rqs.auth;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.rqs.auth.mapper")
 public class ServiceAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceAuthApplication.class, args);
