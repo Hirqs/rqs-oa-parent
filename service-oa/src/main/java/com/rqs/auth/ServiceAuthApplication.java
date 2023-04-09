a10d1741b929074257a30b87d89c68742e39114b
Hirqs <569543446@qq.com>
2023-04-09 19:18:11 +0800

    条件分页查询

--- service-oa/src/main/java/com/rqs/auth/ServiceAuthApplication.java
+++ service-oa/src/main/java/com/rqs/auth/ServiceAuthApplication.java
@@ -3,9 +3,10 @@ package com.rqs.auth;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.rqs.auth.mapper")
+@ComponentScan("com.rqs")
 public class ServiceAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceAuthApplication.class, args);
--- service-oa/src/main/java/com/rqs/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/rqs/auth/controller/SysRoleController.java
@@ -1,12 +1,17 @@
 package com.rqs.auth.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.rqs.auth.service.SysRoleService;
 import com.rqs.common.result.Result;
 import com.rqs.model.system.SysRole;
+import com.rqs.vo.system.SysRoleQueryVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +41,31 @@ public class SysRoleController {
     public Result findAll() {
         List<SysRole> list = sysRoleService.list();
         return Result.ok(list);
+    }
+
+    /**
+     * page:当前页
+     * limit:每一页显示的记录数
+     * sysRoleQueryVo:以对象的形式传递条件
+     */
+    @ApiOperation("条件分页查询")
+    @GetMapping("{page}/{limit}")
+    public Result pageQueryRole(@PathVariable Long page,
+                                @PathVariable long limit,
+                                SysRoleQueryVo sysRoleQueryVo) {
+        //调用service中的方法实现分页
+        // 1.创建Page对象，传递分页相关参数
+        Page<SysRole> pageParam = new Page<>(page, limit);
+        // 2.封装条件，判断条件是否为空，不为空则进行封装
+        LambdaQueryWrapper<SysRole> wrapper = new LambdaQueryWrapper<>();
+        String roleName = sysRoleQueryVo.getRoleName();
+        if (!StringUtils.isEmpty(roleName)) {
+            //封装
+            wrapper.like(SysRole::getRoleName, roleName);
+        }
+        // 3.调用方法进行实现
+        Page<SysRole> pageResult = sysRoleService.page(pageParam, wrapper);
+        return Result.ok(pageResult);
     }
 
 
