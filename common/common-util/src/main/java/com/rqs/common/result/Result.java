c7cbb32ffca96a9925392077d359b85ae56f5dd9
Hirqs <569543446@qq.com>
2023-04-09 17:26:54 +0800

    统一返回结果

---
+++ common/common-util/src/main/java/com/rqs/common/result/Result.java
@@ -0,0 +1,79 @@
+package com.rqs.common.result;
+
+import lombok.Data;
+
+/**
+ * 全局统一返回结果类
+ *
+ */
+@Data
+public class Result<T> {
+
+    private Integer code;//状态码
+    private String message;//返回消息
+    private T data;//返回数据
+
+    public Result(){}
+
+    // 返回数据
+    protected static <T> Result<T> build(T data) {
+        Result<T> result = new Result<T>();
+        if (data != null)
+            result.setData(data);
+        return result;
+    }
+
+    public static <T> Result<T> build(Integer code  , T data, String message) {
+        Result<T> result = build(data);
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+
+    public static <T> Result<T> build(T data, ResultCodeEnum resultCodeEnum) {
+        Result<T> result = build(data);
+        result.setCode(resultCodeEnum.getCode());
+        result.setMessage(resultCodeEnum.getMessage());
+        return result;
+    }
+
+    public static<T> Result<T> ok(){
+        return Result.ok(null);
+    }
+
+    /**
+     * 操作成功
+     * @param data  baseCategory1List
+     * @param <T>
+     * @return
+     */
+    public static<T> Result<T> ok(T data){
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.SUCCESS);
+    }
+
+    public static<T> Result<T> fail(){
+        return Result.fail(null);
+    }
+
+    /**
+     * 操作失败
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static<T> Result<T> fail(T data){
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.FAIL);
+    }
+
+    public Result<T> message(String msg){
+        this.setMessage(msg);
+        return this;
+    }
+
+    public Result<T> code(Integer code){
+        this.setCode(code);
+        return this;
+    }
+}
